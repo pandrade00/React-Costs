aae03c59acfd6323c9aa76ff1db0a5591bc39852
Pedro A <pedrohenriqueandradedesousa.123@hotmail.com>
2026-01-02 17:49:42 -0300

    Criando repositorio

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Empresa from './components/pages/Company'
+import Contato from './components/pages/Contact'
+import NovoProjeto from './components/pages/NewProject'
+import Project from './components/pages/Project'
+
+import Container from './components/layout/Container'
+import NavBar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+
+
+function App() {
+  return (
+    <Router>
+    <NavBar/>
+    <Container customClass="min-height">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/company" element={<Empresa />} />
+        <Route path="/contact" element={<Contato />} />
+        <Route path="/newproject" element={<NovoProjeto />} /> 
+      </Routes>
+    </Container>
+    <Footer/>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/pages/Project.js
@@ -0,0 +1,5 @@
+function Project(){
+    return <h1>Projeto</h1>
+}
+
+export default Project;
