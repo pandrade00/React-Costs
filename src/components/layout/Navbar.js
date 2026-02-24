8be9bc1ef54abaa4e02708719204ce22cf19fe81
Pedro A <pedrohenriqueandradedesousa.123@hotmail.com>
2026-02-24 01:16:39 -0300

    criação do footer e atualizando navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,27 +7,27 @@
 
    function NavBar(){
     return(
-        <nav class={styles.navbar}>
+        <nav className={styles.navbar}>
             <Container>
 
                 <Link to="/">
                 <img src={logo} alt="costs"/>
                 </Link>
 
-                <ul class={styles.list}>
+                <ul className={styles.list}>
                     <li class={styles.item}>
                         <Link to="/">Home</Link>
                     </li>
-                    <li class={styles.item}>
+                    <li className={styles.item}>
                         <Link to="/project">Projetos</Link>
                     </li>
-                    <li class={styles.item}>
+                    <li className={styles.item}>
                         <Link to="/company">Empresa</Link>
                     </li>
-                    <li class={styles.item}>
+                    <li className={styles.item}>
                         <Link to="/newproject">Novo Projeto</Link>
                     </li>
-                    <li class={styles.item}>
+                    <li className={styles.item}>
                         <Link to="/contact">Contato</Link>
                     </li>
                 </ul>
